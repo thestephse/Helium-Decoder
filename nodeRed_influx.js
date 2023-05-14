9880556017c950c99007384c323df357e131a428
Stephan <stephan@fazemodels.com>
2023-05-14 21:58:45 +0200

    additional keys

--- nodeRed_influx.js
+++ nodeRed_influx.js
@@ -21,8 +21,11 @@ let hotspots = msg.payload.hotspots;
 
 for (let i = 0; i < hotspots.length; i++) {
   fields[`hotspot_${i}`] = hotspots[i].name;
-  fields[`rssi_${i}`] = hotspots[i].rssi;
-  fields[`snr_${i}`] = hotspots[i].snr;
+  fields[`hs_rssi_${i}`] = hotspots[i].rssi;
+  fields[`hs_snr_${i}`] = hotspots[i].snr;
+  fields[`hs_lat_${i}`] = hotspots[i].lat;
+  fields[`hs_lon_${i}`] = hotspots[i].long;
+  fields[`hs_reported_${i}`] = hotspots[i].reported_at;
 }
 
 delete fields.hotspots; // Remove the hotspots array from the fields
@@ -30,7 +33,12 @@ delete fields.hotspots; // Remove the hotspots array from the fields
 let influxMessage = {
   measurement: msg.payload.id,
   fields: fields,
-  tags: { name: msg.payload.name },
+  tags: {
+    name: msg.payload.name,
+    organization: msg.payload.metadata.organization_id,
+    multibuy: msg.payload.metadata.multibuy,
+    payload_size: msg.payload.metadata.payload_size,
+  },
   timestamp: new Date(msg.payload.reported_at).getTime() * 1000000, // Nanoseconds
 };
 
